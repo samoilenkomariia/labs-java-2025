22d9c9025d90fab1a3021bd3cd679b6da92f30b0
Samoilenko Mariia <mariia.samoilenko.06@gmail.com>
2025-11-07 21:56:38 +0200

    add main code

--- Lab4/src/main/java/Main.java
+++ Lab4/src/main/java/Main.java
@@ -1,9 +1,40 @@
-import vehicle.PoliceCar;
+import people.Firefighter;
+import people.Person;
+import people.Policeman;
+import vehicle.*;
+
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
         PoliceCar car = new PoliceCar(4);
 //        car.addPassenger(new Firefighter("Tom"));
+
+        Road road = new Road();
+        Vehicle<Person> bus = new Bus(50);
+        bus.addPassenger(new Person("Alice"));
+        bus.addPassenger(new Policeman("Bob"));
+        bus.addPassenger(new Firefighter("Charlie"));
+
+        Vehicle<Policeman> policeCar = new PoliceCar(2);
+        policeCar.addPassenger(new Policeman("Dana"));
+        policeCar.addPassenger(new Policeman("Evan"));
+
+        Vehicle<Firefighter> fireTruck = new FireTruck(6);
+        fireTruck.addPassenger(new Firefighter("Frank"));
+
+        Vehicle<Person> taxi = new Taxi(4);
+        Vehicle<Person> taxi2 = new Taxi(3);
+        taxi2.addPassenger(new Person("John"));
+        taxi2.addPassenger(new Policeman("Mary"));
+        taxi2.addPassenger(new Firefighter("Jane"));
+
+        road.addAllVehicles(List.of(bus, policeCar, fireTruck, taxi, taxi2));
+        road.carsInRoad.forEach(vehicle -> {
+            System.out.println(
+                    vehicle.getClass().getSimpleName() + " - " + vehicle.getOccupiedSeats() + " passengers"
+            );
+        });
     }
 }
